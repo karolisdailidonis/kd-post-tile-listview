6f3c2f2a33f02bc31dfc7e92b17b8d4a22c261d1
Karolis Dailidonnis <mail@karolisdailidonis.de>
2022-07-18 18:45:33 +0200

    bugfixes

--- includes/public_functions.php
+++ includes/public_functions.php
@@ -2,18 +2,17 @@
 /**
  * Change the page template to the selected template on the dropdown
  */
-function kd_post_tile_listview_change_templates($template)
-{
+function kd_post_tile_listview_change_templates($template) {
     if (is_single()) {
         $meta = get_post_meta(get_the_ID());
 
         // if (!empty($meta['_wp_page_template'][0]) && $meta['_wp_page_template'][0] != $template) {
-        if ( str_contains($meta['_wp_page_template'][0], 'single-tiles.php')) {
+        if ( !empty($meta['_wp_page_template'][0]) && str_contains($meta['_wp_page_template'][0], 'single-tiles.php')) {
 
             if (file_exists( $meta['_wp_page_template'][0] ) ) {
                 wp_enqueue_style('kd_post_tile_listview_template_style', KD_POST_TILE_LISTVIEW_PLUGIN_URL . 'templates/assets/single-tiles.css');
                 return $meta['_wp_page_template'][0];
-            } 
+            }
             
             return get_template_directory() . "/single.php";
         }
